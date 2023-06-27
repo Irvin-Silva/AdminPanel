a7e1bc904f9a02f7c0c25633d096fcfb3041954f
IrvinSilva <64101694+sabi-tech@users.noreply.github.com>
2023-06-27 18:34:13 -0500

    Routes

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./List.scss"
+
+
+export const List = () => {
+  return (
+    <div>List</div>
+    
+  )
+}
+export default List
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Single.scss"
+
+export const Single = () => {
+  return (
+    <div>Single</div>
+  )
+}
+export default Single
